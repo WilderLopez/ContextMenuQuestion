42a1e9fe60ec72bad62de41127aa3829565adf62
wilderlopez <wlopezelias@gmail.com>
2021-02-23 18:00:59 -0500

    Initial Commit

---
+++ TestingContextMenuInList/TestingContextMenuInListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingContextMenuInListApp.swift
+//  TestingContextMenuInList
+//
+//  Created by Wilder Lopez on 2/23/21.
+//
+
+import SwiftUI
+
+@main
+struct TestingContextMenuInListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
