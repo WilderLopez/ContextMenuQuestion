ed154ea0d5139834107978dceab0c95fb2cf7fdc
wilderlopez <wlopezelias@gmail.com>
2021-02-23 18:27:51 -0500

    first commit

--- TestingContextMenuInList/ContentView.swift
+++ TestingContextMenuInList/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    let items : [String] = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List{
+            ForEach(items, id: \ .self){ item in
+                LazyVStack{
+                    Row(name: item)
+                }
+            }
+        }
+    }
+}
+
+struct Row : View {
+    let name : String
+    @State var isContext = false
+    var body: some View{
+        Text("\(name) \(isContext ? "appear contextMenu" : "")")
+            .contextMenu {
+                VStack{
+                    Text("Menu Item 1")
+                    Text("Menu Item 2")
+                    Text("Menu Item 3")
+                }.onAppear{
+                    print("appear in contextMenu")
+                    isContext = true
+                }.onDisappear{
+                    print("disappear in contextMenu")
+                    isContext = false
+                }
+            }
     }
 }
 
